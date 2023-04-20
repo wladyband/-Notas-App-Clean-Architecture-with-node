537eed48ec84db2a9b0f6a1ad35bed4e1390ff5e
Wladimir Bandeira <tecnico100@gmail.com>
2023-04-20 12:46:44 -0300

    retirando código comentádo

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -18,26 +18,6 @@ usersRouter.get(
   usersController.show,
 );
 
-// usersRouter.get(
-//   '/',
-//   isAuthenticated,
-//   validateQueryParams,
-//   usersController.show,
-// );
-
-// // Middleware personalizado para validar os parâmetros de consulta
-// function validateQueryParams(req, res, next) {
-//   const { id } = req.query;
-
-//   if (!id || !/^[a-f\d]{8}-([a-f\d]{4}-){3}[a-f\d]{12}$/i.test(id)) {
-//     return res
-//       .status(400)
-//       .json({ message: 'O parâmetro "id" é inválido ou está ausente.' });
-//   }
-
-//   next();
-// }
-
 usersRouter.post(
   '/',
   celebrate({
